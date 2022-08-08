47e545183b864466e854dcae314cc8ef3366e6dd
he-rro <hero.0073722gmail.com>
2022-08-08 08:08:46 -0700

    clear projet folder structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const links=[
+    {name:'Home' , url:"/"},
+    {name:'About' , url:"/about"},
+    {name:'Flights' , url:"/flights"},
+    {name:'Location' , url:"/location"},
+    {name:'Login' , url:"/login"},
+]
+export default function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}

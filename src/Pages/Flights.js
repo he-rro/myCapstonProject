8ada647d00d6a4e14eef9ff67f4c0db77b79abdf
he-rro <hero.0073722gmail.com>
2022-09-18 17:41:50 -0700

    deploy to netlify

--- src/Pages/Flights.js
+++ src/Pages/Flights.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from "../components/Hero";
-import ScrollToTop from "../components/ScrollToTop";
 import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
 import Carousel from '../components/Carousel';
